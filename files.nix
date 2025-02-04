dcea83b5a2c39e5f87355622a40e577877392de8
Will Sturgeon <willstrgn@gmail.com>
2025-02-03 16:53:53 -0800

    Initial commit

---
+++ files.nix
@@ -0,0 +1,138 @@
+{ name, version }:
+{
+
+  ".cargo/config.toml" = ''
+    [target.'cfg(all(target_arch = "arm", target_os = "none"))']
+    # runner = "probe-rs run --chip RP2040 --protocol swd"
+    runner = "elf2uf2-rs -d"
+
+    rustflags = [
+      "-C", "linker=flip-link",
+
+      "-C", "link-arg=--nmagic",
+      "-C", "link-arg=-Tlink.x",
+      "-C", "link-arg=-Tlink-rp.x",
+      "-C", "link-arg=-Tdefmt.x",
+
+      "-C", "inline-threshold=5",
+      "-C", "no-vectorize-loops",
+      "-Z", "trap-unreachable=no",
+    ]
+
+    [build]
+    target = "thumbv6m-none-eabi" # Cortex-M0 and Cortex-M0+
+
+    [env]
+    DEFMT_LOG = "debug"
+  '';
+
+  "build.rs" = ''
+    //! This build script copies the `memory.x` file from the crate root into
+    //! a directory where the linker can always find it at build time.
+    //! For many projects this is optional, as the linker always searches the
+    //! project root directory -- wherever `Cargo.toml` is. However, if you
+    //! are using a workspace or have a more complicated build setup, this
+    //! build script becomes required. Additionally, by requesting that
+    //! Cargo re-run the build script whenever `memory.x` is changed,
+    //! updating `memory.x` ensures a rebuild of the application with the
+    //! new memory settings.
+
+    use std::env;
+    use std::fs::File;
+    use std::io::Write;
+    use std::path::PathBuf;
+
+    fn main() {
+        // Put `memory.x` in our output directory and ensure it's
+        // on the linker search path.
+        let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+        File::create(out.join("memory.x"))
+            .unwrap()
+            .write_all(include_bytes!("memory.x"))
+            .unwrap();
+        println!("cargo:rustc-link-search={}", out.display());
+
+        // By default, Cargo will re-run a build script whenever
+        // any file in the project changes. By specifying `memory.x`
+        // here, we ensure the build script is only re-run when
+        // `memory.x` is changed.
+        println!("cargo:rerun-if-changed=memory.x");
+
+        /*
+        println!("cargo:rustc-link-arg-bins=--nmagic");
+        println!("cargo:rustc-link-arg-bins=-Tlink.x");
+        println!("cargo:rustc-link-arg-bins=-Tlink-rp.x");
+        println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
+        */
+    }
+  '';
+
+  "Cargo.toml" = ''
+    [package]
+    name = "${name}"
+    version = "${version}"
+    edition = "2024"
+
+    [dependencies]
+    cortex-m-rt = { version = "*" }
+    defmt = { version = "*" }
+    defmt-rtt = { version = "*" }
+    embassy-executor = { version = "*", features = [ "arch-cortex-m", "defmt", "executor-thread", "executor-interrupt", "task-arena-size-98304" ] }
+    embassy-rp = { version = "*", features = [ "defmt", "unstable-pac", "time-driver", "critical-section-impl", "rp2040" ] }
+    embassy-time = { version = "*", features = [ "defmt", "defmt-timestamp-uptime" ] }
+    panic-probe = { version = "*", features = [ "print-defmt" ] }
+
+    [profile.dev]
+    debug = 2
+    lto = true
+    opt-level = "z"
+
+    [profile.release]
+    debug = 2
+    lto = true
+    opt-level = "z"
+  '';
+
+  "Cargo.lock" = ''
+    version = 4
+
+    package = []
+  '';
+
+  "memory.x" = ''
+    MEMORY {
+        BOOT2 : ORIGIN = 0x10000000, LENGTH = 0x100
+        FLASH : ORIGIN = 0x10000100, LENGTH = 2048K - 0x100
+
+        /* Pick one of the two options for RAM layout     */
+
+        /* OPTION A: Use all RAM banks as one big block   */
+        /* Reasonable, unless you are doing something     */
+        /* really particular with DMA or other concurrent */
+        /* access that would benefit from striping        */
+        /* RAM   : ORIGIN = 0x20000000, LENGTH = 264K     */
+
+        /* OPTION B: Keep the unstriped sections separate */
+        RAM: ORIGIN = 0x20000000, LENGTH = 256K
+        SCRATCH_A: ORIGIN = 0x20040000, LENGTH = 4K
+        SCRATCH_B: ORIGIN = 0x20041000, LENGTH = 4K
+    }
+
+    EXTERN(BOOT2_FIRMWARE)
+
+    SECTIONS {
+        /* ### Boot loader */
+        .boot2 ORIGIN(BOOT2) :
+        {
+            KEEP(*(.boot2));
+        } > BOOT2
+    } INSERT BEFORE .text;
+  '';
+
+  "rust-toolchain.toml" = ''
+    [toolchain]
+    channel = "nightly"
+    targets = [ "thumbv6m-none-eabi" ]
+  '';
+
+}
---
+++ flake.nix
@@ -0,0 +1,142 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        rust-analyzer-src = {
+          flake = false;
+          url = "github:rust-lang/rust-analyzer/nightly";
+        };
+      };
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nmattia/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-filter.url = "github:numtide/nix-filter";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      fenix,
+      flake-utils,
+      naersk,
+      nix-filter,
+      nixpkgs,
+      self,
+    }:
+
+    let
+
+      name = "pi-pico";
+      version = "0.1.0";
+
+    in
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        env = {
+          CARGO_BUILD_TARGET = "thumbv6m-none-eabi";
+        };
+
+        toolchain =
+          with fenix.packages.${system};
+          combine (
+            (with minimal; [
+              cargo
+              rustc
+            ])
+            ++ (with targets.${env.CARGO_BUILD_TARGET}.latest; [
+              rust-std
+            ])
+          );
+
+        files = import ./files.nix { inherit name version; };
+        write-files = pkgs.lib.strings.concatLines (
+          builtins.attrValues (
+            builtins.mapAttrs (
+              filename: contents:
+              let
+                escaped-filename = "${pkgs.lib.strings.escapeShellArg "./${filename}"}";
+              in
+              ''
+                mkdir -p ${escaped-filename}
+                rm -fr ${escaped-filename}
+                echo ${pkgs.lib.strings.escapeShellArg contents} > ${escaped-filename}
+              ''
+            ) files
+          )
+        );
+
+        with-shebang = script: ''
+          #!${pkgs.bash}/bin/bash
+          set -euo pipefail
+          shopt -s nullglob
+
+          ${script}
+        '';
+
+      in
+      {
+
+        devShells.default = pkgs.mkShell (
+          {
+            packagesFrom = builtins.attrValues self.packages.${system};
+            packages =
+              [ toolchain ]
+              ++ (with pkgs; [
+                elf2uf2-rs
+                flip-link
+                probe-rs
+                rust-analyzer
+              ]);
+          }
+          // env
+        );
+
+        packages = {
+
+          default =
+            (naersk.lib.${system}.override {
+              cargo = toolchain;
+              rustc = toolchain;
+            }).buildPackage
+              (
+                {
+                  inherit name version;
+                  inherit (self.packages.${system}) src;
+                }
+                // env
+              );
+
+          src = pkgs.stdenvNoCC.mkDerivation (
+            {
+              pname = "${name}-src";
+              inherit version;
+
+              src = nix-filter {
+                root = ./.;
+                include = [ ./src ];
+              };
+
+              buildPhase = ":";
+              installPhase = ''
+                mkdir -p $out
+                ls -A | xargs -I{} mv {} $out/
+                cd $out
+                ${write-files}
+              '';
+            }
+            // env
+          );
+
+        };
+
+      }
+    );
+}
